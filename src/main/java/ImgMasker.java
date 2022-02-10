4968615529b5bda289255733e70f0daf7dbdfeac
Aaron Segal <ajs2290@g.rit.edu>
2022-02-09 22:11:44 -0500

    Simulation done

--- src/main/java/ImgMasker.java
+++ src/main/java/ImgMasker.java
@@ -13,8 +13,15 @@ public class ImgMasker {
         nu.pattern.OpenCV.loadLocally();
     }
 
-    public static Scalar low = new Scalar(0,0,0);
-    public static Scalar high = new Scalar(100,150,150);
+    public static final Scalar low = new Scalar(0,0,0);
+    public static final Scalar high = new Scalar(100,150,150);
+
+    public static final int rows = 1;
+    public static final int cols = 1;
+
+    public static final int concurrentThreads = 16;
+    public static final int frames = 60;
+    public static final Mat input = imageToMat("src/main/java/mona_lisa.jpg");
 
     public static Mat imageToMat (String filename) {
         return Imgcodecs.imread(filename);
@@ -26,16 +33,35 @@ public class ImgMasker {
         Mat thresh = new Mat();
         Core.inRange(frameHSV, low,
                 high, thresh);
-        Imgcodecs.imwrite(out, thresh);
+        //Imgcodecs.imwrite(out, thresh);
     }
 
-    public static void main(String[] args) {
-        Mat input = imageToMat("src/main/java/mona_lisa.jpg");
-        Mat [][] part = partition(input,3,3);
-        for (Mat [] matRow : part) {
-            System.out.println(Arrays.toString(matRow));
-        }
+    public static void testThreaded() throws InterruptedException {
+
+        //Mat [][] part = partition(input,rows,cols);
+        long start = System.nanoTime();
         process(input,"src/main/java/mona_lisa_out.jpg");
+        long end = System.nanoTime();
+        //System.out.println("Time to process whole: " + deltaMs(start,end));
+
+//        Thread [] threads = new Thread[rows * cols];
+//        int counter = 0;
+//        for (Mat [] matRow : part) {
+//            for (Mat mat : matRow) {
+//                threads[counter] = new Thread(()->process(mat,""));
+//                counter++;
+//            }
+//        }
+//        start = System.nanoTime();
+//        for (Thread thread : threads) {
+//            thread.start();
+//        }
+//        for (Thread thread : threads) {
+//            thread.join();
+//        }
+//        end = System.nanoTime();
+        //System.out.println("Time to process threads: " + deltaMs(start,end));
+
     }
 
     public static Mat [][] partition (Mat input,int rows, int cols) {
@@ -66,6 +92,41 @@ public class ImgMasker {
             colPixel = 0;
         }
         return toReturn;
+    }
+
+    public static double deltaMs(long start,long end){
+        return (end - start) / 1000000.0;
+    }
+
+    public static void nConcurrent() throws InterruptedException {
+        Thread [] concurrent = new Thread[concurrentThreads];
+        for ( int i = 0 ; i < concurrentThreads ; i ++ ) {
+                concurrent[i] = new Thread(()-> {
+                    try {
+                        testThreaded();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        long start = System.nanoTime();
+        for (Thread thread : concurrent) {
+            thread.start();
+        }
+        for (Thread thread : concurrent) {
+            thread.join();
+        }
+        long end = System.nanoTime();
+        //System.out.println("Time to process " + concurrentThreads + " images: " + deltaMs(start,end));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        long start = System.nanoTime();
+        for ( int i = 0 ; i < frames ; i ++ ) {
+            nConcurrent();
+        }
+        long end = System.nanoTime();
+        System.out.println("Time to process "+ frames +" frames from " + concurrentThreads + " sources:  " + deltaMs(start,end) + "ms");
     }
 
 
